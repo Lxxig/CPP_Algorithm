eac99881d0dc840e85590ea869c595e23aea1aa3
Lxxig <mayong0707@gmail.com>
2024-11-15 12:26:27 +0900

    Algorithm_Study

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+	cin.ignore();
+	string Input;
+
+	for (int i = 0; i < T; i++)
+	{
+		getline(cin, Input);
+	}
+
+	return 0;
+}
