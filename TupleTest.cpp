657486b4c43d7952ea123919ef173049e209f7ea
Lxxig <mayong0707@gmail.com>
2024-11-15 20:50:18 +0900

    tuple vector의 입출력

--- TupleTest.cpp
+++ TupleTest.cpp
@@ -12,19 +12,19 @@ int main()
     string firstName, LastName;
     int age;
 
-    for (int i = 0; i < N; ++i)
+    for (int i = 0; i < VectorTuple.capacity(); ++i)
     {
-        /*cin >> get<0>(VectorTuple[i]);
-        cin >> get<1>(VectorTuple[i]);
-        cin >> get<2>(VectorTuple[i]);*/
         cin >> LastName >> firstName >> age;
+
         VectorTuple[i] = make_tuple(LastName, firstName, age);
+        // VectorTuple.push_back(make_tuple( LastName, firstName, age ));
+        // 주석처리한 tuple vector 입력의 경우 VectorTuple의 사이즈를
+        // 처음에 정해주지 않은 상태에서 for문을 이용할 때 좋다.
+        //
     }
 
     for (auto str : VectorTuple)
     {
-        cout << get<1>(str);
-        cout << get<0>(str);
-        cout << get<2>(str) << "\n";
+        cout << get<0>(str) << " " << get<1>(str) << " " << get<2>(str) << "\n";
     }
 }
